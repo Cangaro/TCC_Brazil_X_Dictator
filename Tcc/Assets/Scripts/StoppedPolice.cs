42aeff0cad0d22538ed6d974c742c8071c81a59b
Cangaro <117197711+Cangaro@users.noreply.github.com>
2023-11-16 17:48:22 -0300

    Penultimo dia

--- Tcc/Assets/Scripts/StoppedPolice.cs
+++ Tcc/Assets/Scripts/StoppedPolice.cs
@@ -1,18 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class StoppedPolice : MonoBehaviour
 {
+    private Animator _animator;
+
+    private Rigidbody2D rig;
+
+    public int roubodepontos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _animator = GetComponent<Animator>();
+        rig = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
-}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            _animator.Play("PolliceFire");
+            collision.gameObject.GetComponent<FamousPoints>().Pontosperder(roubodepontos);
+        }
+    }
+   }
